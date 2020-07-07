b4df441225621b706c64e6c4e810e5c81b74a637
Manjul,Mohit(mm0690) <mm0690@att.com>
2020-07-07 21:44:45 +0530

    updated to allow running particular steps.

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# workman
+# workman - Workflow Manager
+A minimalist, lightweight workflow manager.
+Create a Workflow as a YAML file and pass the path as the first positional argument.
+Refer to the sample Workflow YAML file below.
+
+#### Things to remember:
+- The steps will be executed sequentially in ascending order one at a time.
+- There is an option to pass the selective steps to be executed. Refer to the usage section below.
+- Every run will get a unique runId in the format: YYYYMMDDHH24MMSS.
+- Install the requirements before running the script to avoid errors.
+
+#### Sample Workflow File:
+@[:code_block](./wf_schema.yaml)
+
+#### Run Command:
+    usage: runner.py [-h] [-l LOGDIR] [-s STEPS] [-v] wfFile
+
+#### Usage:
+
+##### positional arguments:
+    - wfFile:
+      Workflow yaml file that is to be executed by the runner.
+
+##### optional arguments:
+    -h, --help
+      Display Help
+    -l LOGDIR, --logDir LOGDIR
+    Log file directory. Passing this will implicitly enable logging the   output to a file.
+      Log file will be in the format: "runnerlog.WorkflowName.runId.log"
+    -s STEPS, --steps STEPS
+      Steps to execute. If not passed, all the steps will be executed.
+      2 Formats supported:
+        --> 'ST-EN'. Example: 1-3, 2-5. Both the values need to be a positive   integer number.
+        --> 'STEPNO, {STEPNO,}'. Example: 1,3,5. All the step numbers needs to be   positive integer numbers.
+      Steps will *always* be executed sequentially in ascending order.
+    -v, --verbose
+      Increase log verbosity.
+
+#### Requirements:
+@[:code_block](./requirements.txt)
+
+##### To install the requirements:
+    pip install -r requirements.txt
--- runner.py
+++ runner.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import sys, yaml, subprocess
 from argparse import Namespace, ArgumentParser
 from os import path
@@ -10,13 +11,54 @@ wf={}
 conf={}
 status={}
 
+def exitWithError(message):
+  print(message)
+  print("Exiting with error...")
+  exit(99)
+
+def getSteps():
+  global conf
+  _steps=[]
+  
+  if args.steps:
+    if "-" in args.steps:
+      print("Steps passed as ST-EN")
+      s = args.steps.split("-")
+      if (len(s) != 2):
+        exitWithError("Only 2 values are supported when using ST-EN format.")
+      else:
+        try:
+          _FROM=int(s[0])
+          _TO=int(s[1])
+        except ValueError:
+          exitWithError("Non numeric step number passed. Please check.")
+      _steps=list(range(_FROM, _TO + 1))
+    elif "," in args.steps:
+      print("Steps passed as list")
+      for s in args.steps.split(","):
+        if s.strip() != "":
+          try:
+            _steps.append(int(s))
+          except ValueError:
+            exitWithError("Non numeric step number passed. Please check.")
+      _steps = list(dict.fromkeys(_steps))
+      _steps.sort()
+    else:
+      exitWithError("Incorrect format of steps. Check usage for supported formats.")
+  else:
+    _FROM=1
+    _TO=len(wf["Tasks"]) + 1
+    _steps=list(range(_FROM, _TO))
+  
+  conf["steps"]=_steps
+
 def runSubProcess(command):
   OUT=subprocess.run(command, shell=True, capture_output=True, encoding="utf-8")
   if OUT.returncode:
     if args.verbose:
-      raise Exception(OUT.stdout + "\n" + OUT.stderr)
+      exitWithError(OUT.stdout + "\n" + OUT.stderr)
     else:
-      raise Exception(OUT.stdout)
+      exitWithError(OUT.stdout)
   if args.verbose:
     print(OUT.stdout)
 
@@ -25,7 +67,8 @@ def parseCmd():
   parser = ArgumentParser()
   parser.add_argument("wfFile", type=str, help="Workflow yaml file that is to be executed by the runner.")
   parser.add_argument("-l", "--logDir", type=str, help="Log file directory. Passing this will implicitly enable logging the output to a file.")
-  parser.add_argument("-v", "--verbose", action="store_true", help="Increase error log verbosity.")
+  parser.add_argument("-s", "--steps", type=str, help="Steps to execute. 2 Formats supported: 'ST-EN', 'STEPNO, {STEPNO,}'.")
+  parser.add_argument("-v", "--verbose", action="store_true", help="Increase log verbosity.")
   args = parser.parse_args()
 
 def initialize():
@@ -35,7 +78,7 @@ def initialize():
     sys.tracebacklimit=1000
   
   if not path.isfile(args.wfFile):
-    raise Exception("Workflow yaml file [" + args.wfFile +"] is not accessible.")
+    exitWithError("Workflow yaml file [" + args.wfFile +"] is not accessible.")
   else:
     print("Parsing [" + args.wfFile + "] and validating format.")
     runSubProcess("pykwalify -d " + args.wfFile + " -s WF_Schema.yaml")
@@ -51,24 +94,30 @@ def initialize():
     print("Initializing Log File.")
     conf["logFileName"] = "runnerlog." + wf["WorkflowName"] + "." + conf["runId"] + ".log"
     if not path.isdir(args.logDir):
-      raise Exception("[" + args.logDir +"] does not exist or is not a directory.")
+      exitWithError("[" + args.logDir +"] does not exist or is not a directory.")
     else:
       setLogFile(args.logDir + "/" + conf["logFileName"])
 
 def executeWorkflow():
   global status
+  i=0
   for task in wf["Tasks"]:
-    _task=task.copy()
-    print("Executing task : " + task["TaskName"] + " : " + task["RunCommand"])
-    _task["StartTime"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    runSubProcess(task["RunCommand"])
-    _task["EndTime"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    status["Tasks"].append(_task)
+    i+=1
+    if i in conf["steps"]:
+      _task=task.copy()
+      print("Executing task : " + task["TaskName"] + " : " + task["RunCommand"])
+      _task["StartTime"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+      runSubProcess(task["RunCommand"])
+      _task["EndTime"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+      status["Tasks"].append(_task)
 
 if __name__ == "__main__":
   parseCmd()
   initialize()
   print("Starting execution of Workflow '{wfname}' with runid '{runid}'...".format(wfname=wf["WorkflowName"], runid=conf["runId"]))
+  if args.steps:
+    print("Steps specified for execution: '{steps}'...".format(steps=args.steps))
+  getSteps()
   status["WorkflowName"] = wf["WorkflowName"]
   status["Description"] = wf["Description"]
   status["runId"] = conf["runId"]
@@ -78,3 +127,5 @@ if __name__ == "__main__":
   status["EndTime"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
   print(yaml.dump(status))
   print("Execution completed Successfully...")
+else:
+  exitWithError("This is not a module. Please check the usage...")
